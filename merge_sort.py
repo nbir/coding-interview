8f1db7d48efcd07f881031d17bb67ea7bbaac5eb
Nibir Bora <nibirbora@gmail.com>
2018-12-24 11:19:13 -0800

    Merge sort changes - initialize temp array in merge function

--- merge_sort.py
+++ merge_sort.py
@@ -2,51 +2,50 @@ import math
 
 
 def merge_sort(array):
-    temp_array = [0] * len(array)
-    _merge_sort(array, temp_array, 0, len(array) - 1)
+    _merge_sort(array, 0, len(array) - 1)
 
 
-def _merge_sort(array, temp_array, from_index, to_index):
+def _merge_sort(array, from_index, to_index):
     if (from_index >= to_index): return
 
     mid_index = math.floor((from_index + to_index) / 2)
 
-    _merge_sort(array, temp_array, from_index, mid_index)
-    _merge_sort(array, temp_array, mid_index + 1, to_index)
+    _merge_sort(array, from_index, mid_index)
+    _merge_sort(array, mid_index + 1, to_index)
 
-    merge(array, temp_array, from_index, mid_index, to_index)
+    merge(array, from_index, mid_index, to_index)
 
 
-def merge(array, temp_array, from_index, mid_index, to_index):
-    i = from_index
-    j = mid_index + 1
-    k = from_index
+def merge(array, from_index, mid_index, to_index):
+    # Copy items into temporary arrays
+    left_array = array[from_index:mid_index + 1]
+    right_array = array[mid_index + 1:to_index + 1]
 
-    # Merge
-    while i <= mid_index and j <= to_index:
-        if array[i] < array[j]:
-            temp_array[k] = array[i]
-            i += 1
+    left_index = 0
+    right_index = 0
+    index = from_index
+
+    # Sorted merge
+    while left_index < len(left_array) and right_index < len(right_array):
+        if left_array[left_index] < right_array[right_index]:
+            array[index] = left_array[left_index]
+            left_index += 1
         else:
-            temp_array[k] = array[j]
-            j += 1
+            array[index] = right_array[right_index]
+            right_index += 1
 
-        k += 1
+        index += 1
 
     # Copy remaining items
-    while i <= mid_index:
-        temp_array[k] = array[i]
-        i += 1
-        k += 1
+    while left_index < len(left_array):
+        array[index] = left_array[left_index]
+        left_index += 1
+        index += 1
 
-    while j <= to_index:
-        temp_array[k] = array[k]
-        j += 1
-        k += 1
-
-    # Copy content from temp_array to original array
-    for l in range(from_index, to_index + 1):
-        array[l] = temp_array[l]
+    while right_index < len(right_array):
+        array[index] = right_array[right_index]
+        right_index += 1
+        index += 1
 
 
 def test():
@@ -57,12 +56,16 @@ def test():
     merge_sort(empty_array)
     print("passed") if empty_array == [] else print("failed")
 
+    # Unit length array
+    array = [randint(0, 10000)]
+    _array = array[:]
+    merge_sort(array)
+    print("passed") if array == _array else print("failed")
+
     # Non-empty array
     array = [randint(0, 10000) for i in range(0, 1000)]
     _array = array[:]
-
     merge_sort(array)
-
     print("passed") if array == sorted(_array) else print("failed")
 
 
